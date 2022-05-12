0d04b86819f1b9096e32b33f6554958ace3ecab5
Xiaokang Chen <xiaokang.chen888@gmail.com>
2022-05-11 22:31:54 -1000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205-final-exam - EE 205 - Spr 2022
+///
+/// @file main.cpp
+/// @version 1.0
+///
+/// @author Xiaokang Chen <xiaokang@hawaii.edu>
+/// @date   11_May_2022
+///////////////////////////////////////////////////////////////////////////////
+
+
+#include <iostream>
+
+
+int main() {
+
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+
+}
