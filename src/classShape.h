6cf67b6b8a4d65867913c86199f1895b764809c3
Xiaokang Chen <xiaokang.chen888@gmail.com>
2022-05-13 10:46:39 -1000

    Finished classSquare

---
+++ src/classShape.h
@@ -0,0 +1,23 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205-final-exam - EE 205 - Spr 2022
+///
+/// @file classShape.h
+/// @version 1.0
+///
+/// @author Xiaokang Chen <xiaokang@hawaii.edu>
+/// @date   13_May_2022
+///////////////////////////////////////////////////////////////////////////////
+
+
+#pragma once
+
+
+class classShape {
+
+    public:
+    ///// Virtual Abstract Method /////
+        virtual double compute_area() noexcept = 0 ;
+    ///// Virtual Abstract Method /////
+
+} ;
---
+++ src/classSquare.cpp
@@ -0,0 +1,20 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205-final-exam - EE 205 - Spr 2022
+///
+/// @file classSquare.cpp
+/// @version 1.0
+///
+/// @author Xiaokang Chen <xiaokang@hawaii.edu>
+/// @date   13_May_2022
+///////////////////////////////////////////////////////////////////////////////
+
+
+#include "classSquare.h"
+
+
+double classSquare::getSide() {
+
+    return classRectangle::length ;
+
+}
